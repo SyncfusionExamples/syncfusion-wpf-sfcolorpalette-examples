ad444beecb4ba306d04d4951259631ad47b9391c
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-05-28 10:47:32 +0530

    SfColorPalette samples committed.

---
+++ SfColorPalette/Appearance/Converter/ColorToBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Appearance
+{
+    public class ColorToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (null == value)
+                return null;
+            Color color = (Color)value;
+            return new SolidColorBrush(color);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return true;
+        }
+    }
+}
---
+++ SfColorPalette/Appearance/ViewModel/ViewModel.cs
@@ -0,0 +1,59 @@
+﻿
+
+using Syncfusion.Windows.Shared;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Appearance
+{
+    class ViewModel : NotificationObject
+    {
+        private FlowDirection flowDirection;
+        private Brush background = Brushes.LightYellow;
+        private Brush borderBrush = Brushes.DarkOrange;
+        private Brush foreground = Brushes.Red;
+
+        public Brush Background
+        {
+            get { return background; }
+            set
+            {
+                background = value;
+                this.RaisePropertyChanged(nameof(Background));
+            }
+        }
+        public Brush BorderBrush
+        {
+            get { return borderBrush; }
+            set
+            {
+                borderBrush = value;
+                this.RaisePropertyChanged(nameof(BorderBrush));
+            }
+        }
+        public Brush Foreground
+        {
+            get { return foreground; }
+            set
+            {
+                foreground = value;
+                this.RaisePropertyChanged(nameof(Foreground));
+            }
+        }
+
+        public FlowDirection FlowDirection
+        {
+            get { return flowDirection; }
+            set
+            {
+                flowDirection = value;
+                this.RaisePropertyChanged(nameof(FlowDirection));
+            }
+        }
+
+        public ViewModel()
+        {
+
+        }
+    }
+}
